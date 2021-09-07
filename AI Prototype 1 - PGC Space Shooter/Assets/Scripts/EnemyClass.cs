807eda7c2f07c5f4069263cbfebe055f7d505354
Jens <je2ba@outlook.com>
2021-09-07 11:46:12 +0200

    Getting this party started

---
+++ AI Prototype 1 - PGC Space Shooter/Assets/Scripts/EnemyClass.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyClass
+{
+    public void Onspawn()
+    {
+
+    }
+
+    public void OnDeath()
+    {
+
+    }
+
+    public void Shoot()
+    {
+
+    }
+}
---
+++ AI Prototype 1 - PGC Space Shooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Move()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
