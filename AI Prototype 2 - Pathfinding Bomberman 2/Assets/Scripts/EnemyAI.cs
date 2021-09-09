382f351dadbd32cd7b5ddb71cbc9dfb002ff4e7c
Jens <je2ba@outlook.com>
2021-09-09 15:02:03 +0200

    Getting bombastic up in here

---
+++ AI Prototype 2 - Pathfinding Bomberman 2/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    // Use search method to find best possible pathfinding options, choose one of the options
+
+
+}
